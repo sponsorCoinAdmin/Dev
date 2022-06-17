bf770415a2191b7a4987c8e100d6a4af29a176f8
sponsorCoinAdmin <sponsorcoin@protonmail.com>
2022-06-17 01:51:01 -0400

    token Selector WIP

--- V2/sponsorCoinEthers_JS/utils/TokenSelector.js
+++ V2/sponsorCoinEthers_JS/utils/TokenSelector.js
@@ -1,21 +1,41 @@
 class TokenSelector {
   constructor (select_ID) {
-    this.tokenSelect = document.getElementById(select_ID);;
-  }
-
-  test() {
-    alert("TokenSelector Test");
+    this.selector = document.getElementById(select_ID);
+    this.selector_TX = document.getElementById(select_ID.replace("_SEL", "_TX"));
   }
 
   buttonPressed() {
-    this.processSelectedToken(this.tokenSelect.id);
+    this.processSelectedToken1();
   }
 
-  processSelectedToken(selectId) {
-    var tokenSelect = document.getElementById(selectId);
-    var selIdx = tokenSelect.selectedIndex;
-    processSelectedTokenIndex(selectId, selIdx);
+  processSelectedToken1() {
+    var selIdx = this.selector.selectedIndex;
+    this.processSelectedTokenIndex1(this.selector.id, selIdx);
+    processSelectedTokenIndex(this.selector.id, selIdx);
   }
+
+  processSelectedTokenIndex1(selectId, selIdx) {
+    // var tokenSelect = document.getElementById(selectId);
+    // if (selIdx == 0) {
+    //   GUI_OpenAddCryptoForm(tokenSelect);
+    //   this.selector_TX.value = "";
+    // }
+    // else {
+    //   activeTokenIndex = tokenSelect.selectedIndex = selIdx;
+    //   selOption = tokenSelect.options[selIdx];
+    //   tokenText = selOption.text;
+    //   address = selOption.value;
+    //   tokenValue = getTokenProperty(address, SelectorPropertyKey);
+  
+    //   if (isEmpty(tokenValue))
+    //     tokenValue = address;
+  
+    //   // Populate Address Text Field
+    //   this.selector_TX.value = tokenValue;
+    // }
+  }
+ 
+
 }
 
   function processSelectedTokenIndex(selectId, selIdx) {
